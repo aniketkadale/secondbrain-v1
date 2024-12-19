866484663e0772bd4dbf5014226ecc4d23ff2880
ANIKET KADALE <aniketkadale18@gmail.com>
2024-12-19 22:19:07 +0530

    navbar updated

--- secondbrain-fe/src/components/Navbar.tsx
+++ secondbrain-fe/src/components/Navbar.tsx
@@ -1,16 +1,20 @@
-
-
 const Navbar = () => {
   return (
-    <div className="flex bg-gradient-to- ">
+    <div className="flex justify-between items-center w-full">
       <div className="flex justify-center items-center gap-2 p-4">
         <img className="h-12 text-white" src="logo_white.png" alt="logo" />
         <h1 className="font-poppins text-2xl text-white font-bold">
           Second Brain
         </h1>
       </div>
+
+      <div className="m-4">
+        <a href="https://github.com/aniketkadale/secondbrain-v1" target="_blank">
+          <img className="h-10" src="/github-mark.png" alt="Github" />
+        </a>
+      </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
--- secondbrain-fe/src/pages/Auth.tsx
+++ secondbrain-fe/src/pages/Auth.tsx
@@ -28,44 +28,51 @@ export const Signup = () => {
 
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-purple-600">
-      <div className="shadow-2xl">
-        <img
-          className="h-96 rounded-tl-md rounded-bl-md"
-          src="/sign_up_png.png"
-          alt="sign up"
-        />
-      </div>
-      <div
-        className="bg-white h-96 rounded-tr-md rounded-br-md
-       flex items-center justify-center flex-col p-8 shadow-2xl"
-      >
-        <div className="flex justify-start mb-2">
-          <h1 className="text-xl font-semibold">Welcome Back!</h1>
-        </div>
-        <Input reference={usernameRef} type="text" placeholder="Username" />
-        <Input reference={passwordRef} type="password" placeholder="Password" />
-        <div className="w-full mt-4">
-          <Button
-            variant="primary"
-            size="md"
-            text="Signup"
-            fullWidth={true}
-            onClick={handleSignup}
+      <div className="flex border-1 rounded-md shadow-2xl">
+        <div>
+          <img
+            className="h-96 rounded-tl-md rounded-bl-md"
+            src="/auth.png"
+            alt="sign up"
           />
         </div>
+        <div
+          className=" h-96 rounded-tr-md rounded-br-md
+       flex items-center justify-center flex-col p-8"
+        >
+          <div className="flex flex-col text-center font-poppins justify-start mb-2">
+            <h1 className="text-2xl text-purple-600 font-bold">Sign Up</h1>
+            <h1 className="text-xl ">Welcome Back!</h1>
+          </div>
+          <Input reference={usernameRef} type="text" placeholder="Username" />
+          <Input
+            reference={passwordRef}
+            type="password"
+            placeholder="Password"
+          />
+          <div className="w-full mt-4">
+            <Button
+              variant="primary"
+              size="md"
+              text="Signup"
+              fullWidth={true}
+              onClick={handleSignup}
+            />
+          </div>
 
-        <div>
-          <span className="text-xs">
-            Already have an account?{" "}
-            <span
-              className="cursor-pointer hover:text-[#929DAD] underline"
-              onClick={() => {
-                navigate("/signin");
-              }}
-            >
-              Sign in
+          <div>
+            <span className="text-xs">
+              Already have an account?{" "}
+              <span
+                className="cursor-pointer text-blue-600 hover:text-blue-500 underline"
+                onClick={() => {
+                  navigate("/signin");
+                }}
+              >
+                Sign in
+              </span>
             </span>
-          </span>
+          </div>
         </div>
       </div>
     </div>
@@ -97,42 +104,49 @@ export const Signin = () => {
   };
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gradient-to-r from-purple-600">
-      <div className="bg-white h-96 rounded-tl-md rounded-bl-md flex items-center justify-center flex-col p-8">
-        <div className="flex justify-start mb-2 shadow-2xl">
-          <h1 className="text-xl font-semibold">Welcome Back!</h1>
+      <div className="flex border-1 rounded-md shadow-2xl shadow-l-none">
+        <div className=" h-96 rounded-tl-md rounded-bl-md flex items-center justify-center flex-col p-8">
+          <div className="flex flex-col text-center font-poppins justify-start mb-2">
+            <h1 className="text-2xl text-purple-600 font-bold">Sign In</h1>
+            <h1 className="text-xl ">Welcome Back!</h1>
+          </div>
+          <Input reference={usernameRef} type="text" placeholder="Username" />
+          <Input
+            reference={passwordRef}
+            type="password"
+            placeholder="Password"
+          />
+          <div className="w-full mt-4">
+            <Button
+              variant="primary"
+              size="md"
+              text="Signin"
+              fullWidth={true}
+              onClick={handleSignin}
+            />
+          </div>
+          <div>
+            <span className="text-xs">
+              Don't have an account?{" "}
+              <span
+                className="cursor-pointer text-blue-600 hover:text-blue-500 underline"
+                onClick={() => {
+                  navigate("/signup");
+                }}
+              >
+                Sign up
+              </span>
+            </span>
+          </div>
         </div>
-        <Input reference={usernameRef} type="text" placeholder="Username" />
-        <Input reference={passwordRef} type="password" placeholder="Password" />
-        <div className="w-full mt-4">
-          <Button
-            variant="primary"
-            size="md"
-            text="Signin"
-            fullWidth={true}
-            onClick={handleSignin}
+
+        <div>
+          <img
+            className="h-96 rounded-tr-md rounded-br-md"
+            src="/auth.png"
+            alt="sign up"
           />
         </div>
-        <div>
-          <span className="text-xs">
-            Don't have an account?{" "}
-            <span
-              className="cursor-pointer hover:text-[#929DAD] underline"
-              onClick={() => {
-                navigate("/signup");
-              }}
-            >
-              Sign up
-            </span>
-          </span>
-        </div>
-      </div>
-
-      <div className="shadow-2xl">
-        <img
-          className="h-96 rounded-tr-md rounded-br-md"
-          src="/sign_up_png.png"
-          alt="sign up"
-        />
       </div>
     </div>
   );

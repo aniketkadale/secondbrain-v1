70478e3653a78b784ead66e6d30533a7c4ee6889
ANIKET KADALE <aniketkadale18@gmail.com>
2024-12-22 11:23:38 +0530

    changes made in auth loading

--- secondbrain-fe/src/interfaces/ButtonProps.ts
+++ secondbrain-fe/src/interfaces/ButtonProps.ts
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 export interface ButtonProps {
   variant: "primary" | "secondary" | "danger";
   size: "sm" | "md" | "lg";
-  text?: string;
+  text?: string | ReactElement;
   startIcon?: ReactElement;
   endIcon?: ReactElement;
   onClick?: () => void;
